3801e41ee373f3163abb8bf25213d51e797629fc
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-25 03:47:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,9 @@
 def my_each(collection)
   i = 0 
   while i < collection.length
-  yield collection[i]
-  i += 1 
-  
-  # code here
+    yield collection[i]
+    i += 1 
+  else 
+    puts "There is nothing"
+  end 
 end
